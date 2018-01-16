e5f978d16182fd1868479a7d3fba72d492b0d8cd
HuangYuanAvril <HuangYuanAvril@hotmail.com>
2018-01-16 22:56:58 +0800

    新增当连续三次登陆失败时的输出提示

--- login.java
+++ login.java
@@ -9,25 +9,28 @@ public class login {
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("-----注册-----");
-		System.out.println("请输入用户名：");
+		System.out.println("---------注册---------");
+		System.out.print("请输入用户名：");
 		String name = sc.nextLine();
-		System.out.println("请输入密码：");
+		System.out.print("请输入密码：");
 		String password = sc.nextLine();
 		System.out.println("注册成功！");
 		
-		System.out.println("-----登陆-----");
+		System.out.println("---------登陆----------");
 		for (int count  = 3; count > 0; count--) {
 			System.out.println("还有" + count + "机会");
-			System.out.println("请输入用户名(请注意区别大小写)：");
+			System.out.print("请输入用户名(请注意区别大小写)：");
 			String name_temp = sc.nextLine();
-			System.out.println("请输入密码（请注意区分大小写）：");
+			System.out.print("请输入密码（请注意区分大小写）：");
 			String password_temp = sc.nextLine();
 			if (name_temp.equals(name) && password_temp.equals(password)) {
 				System.out.println("登陆成功！");
 				break;
 			} else {
 				System.out.println("登录名或密码输入错误！");
+				if (count == 0) {
+					System.out.println("今日登陆机会已用完，请明日再来！");
+				}
 			}
 		}
 		
